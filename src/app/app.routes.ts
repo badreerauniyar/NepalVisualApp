6c9c9c7af5fe3eec1a17ed0a9a7e63ef078c5762
Badri Rauniyar <badree.rauniyar@gmail.com>
2025-11-30 23:12:19 +0545

    route changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,21 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+  // {
+  //   path: '',
+  //   loadChildren: () => import('./modules/homepage/homepage-module').then(m => m.HomepageModule),
+  //   title: 'Nepal Visual - Geographic & School Mapping'
+  // },
   {
     path: '',
-    loadChildren: () => import('./modules/homepage/homepage-module').then(m => m.HomepageModule),
-    title: 'Nepal Visual - Geographic & School Mapping'
-  },
-  {
-    path: 'voter-list',
     loadChildren: () => import('./modules/voter-list/voter-list-module').then(m => m.VoterListModule),
     title: 'Nepal Visual - Geographic & School Mapping'
   },
-  {
-    path: 'home',
-    redirectTo: '',
-    pathMatch: 'full'
-  },
+  // {
+  //   path: 'home',
+  //   redirectTo: '',
+  //   pathMatch: 'full'
+  // },
   {
     path: '**',
     redirectTo: ''
